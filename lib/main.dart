551f566ac9d66d0b106f0eb92af5e7ca3f43ef22
EhsanTgv <EhsanTgv@gmail.com>
2023-03-30 17:38:17 +0300

    Implement parallax

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,105 +9,140 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Parallax Demo',
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(body: Parallax()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Parallax extends StatefulWidget {
+  const Parallax({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Parallax> createState() => _ParallaxState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ParallaxState extends State<Parallax> {
+  late PageController _pageController;
+  late double _pageOffset;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _pageOffset = 0;
+    _pageController = PageController(initialPage: 0);
+    _pageController.addListener(
+        () => setState(() => _pageOffset = _pageController.page ?? 0));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+    return Stack(
+      children: [
+        BackgroundImage(
+          pageCount: screens.length + 1,
+          screenSize: MediaQuery.of(context).size,
+          offset: _pageOffset,
+        ),
+        PageView(
+          controller: _pageController,
+          children: [
+            ...screens
+                .map((screen) => Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  color: Colors.black.withOpacity(0.4),
+                  child: Text(
+                    screen.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 80,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  color: Colors.black.withOpacity(0.4),
+                  child: Text(
+                    screen.body,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ))
+                .toList(),
           ],
         ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+}
+
+class BackgroundImage extends StatelessWidget {
+  const BackgroundImage({
+    Key? key,
+    required this.pageCount,
+    required this.screenSize,
+    required this.offset,
+  }) : super(key: key);
+
+  final Size screenSize;
+  final int pageCount;
+  final double offset;
+
+  @override
+  Widget build(BuildContext context) {
+    int lastPageIdx = pageCount - 1;
+    int firstPageIdx = 0;
+    int alignmentMax = 1;
+    int alignmentMin = -1;
+    int pageRange = (lastPageIdx - firstPageIdx) - 1;
+    int alignmentRange = (alignmentMax - alignmentMin);
+    double alignment =
+        (((offset - firstPageIdx) * alignmentRange) / pageRange) + alignmentMin;
+
+    return SizedBox(
+      height: screenSize.height,
+      width: screenSize.width,
+      child: Image(
+        image: const AssetImage('assets/tokyo-street-pano.jpg'),
+        alignment: Alignment(alignment, 0),
+        fit: BoxFit.fitHeight,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class Screen {
+  const Screen({required this.title, required this.body});
+
+  final String title;
+  final String body;
+}
+
+const List<Screen> screens = [
+  Screen(title: '夜', body: 'Night'),
+  Screen(title: '通り', body: 'Street'),
+  Screen(title: 'ネオン', body: 'Neon sign'),
+  Screen(title: '舗', body: 'Store'),
+  Screen(title: '東京', body: 'Tokyo'),
+  Screen(title: '夜', body: 'Night'),
+  Screen(title: '通り', body: 'Street'),
+  Screen(title: 'ネオン', body: 'Neon sign'),
+  Screen(title: '舗', body: 'Store'),
+  Screen(title: '東京', body: 'Tokyo'),
+];
